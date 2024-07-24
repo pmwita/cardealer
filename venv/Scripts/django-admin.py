c12dd2377cb72b5e9628fc3b52db6fd57ab22efa
pmwita <pmwita2017@gmail.com>
2024-07-24 12:34:17 +0300

    Add all my files

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\xampp8240\htdocs\cardealer\venv\Scripts\python.exe
+#!C:\xampp\htdocs\cardealer\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
